52ac6cb175c262011f4edf97f8332eb935b754fa
madison sorah <madisonsorah@gmail.com>
2021-11-02 09:29:41 -0400

    commit tree 27474faa2de66c3624c414984a0d29038d295999

---
+++ index.js
@@ -0,0 +1,9 @@
+const divide = function(num1 = 2000, num2 = 100) {
+        return num1 / num2;
+    } 
+    divide();
+    
+    const square = (x = 2) => x * x;
+
+    const add = (a = 3, b = 4) => a + b;
+
